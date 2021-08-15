47bd4a8136cdb25705c70b0279fe2dac55e719b5
Serhii Havryliuk <havserhii@gmail.com>
2021-08-16 02:40:27 +0300

    add version 3

---
+++ ver.3/src/Frontend/Assets.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace WPPunk\Subscribe\Frontend;
+
+/**
+ * Assets class.
+ */
+class Assets {
+
+	/**
+	 * An action name for the subscription nonce.
+	 */
+	public const SUBSCRIBE_NONCE_ACTION = 'subscribe-action';
+
+	/**
+	 * Add hooks.
+	 */
+	public function add_hooks(): void {
+
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_styles' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_scripts' ] );
+	}
+
+	/**
+	 * Register styles.
+	 */
+	public function register_styles(): void {
+
+		wp_register_style(
+			'subscribe',
+			SUBSCRIBE_URL . '/assets/css/main.css',
+			[],
+			SUBSCRIBE_VERSION
+		);
+	}
+
+	/**
+	 * Register scripts.
+	 */
+	public function register_scripts(): void {
+
+		wp_register_script(
+			'subscribe',
+			SUBSCRIBE_URL . '/assets/js/main.js',
+			[],
+			SUBSCRIBE_VERSION,
+			true
+		);
+
+		wp_localize_script(
+			'subscribe',
+			'subscribe',
+			[
+				'adminUrl' => admin_url( 'admin-ajax.php' ),
+				'nonce'    => wp_create_nonce( self::SUBSCRIBE_NONCE_ACTION ),
+			]
+		);
+	}
+
+}
---
+++ ver.3/subscribe.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Subscribe form and list of subscribers.
+ *
+ * Plugin Name:         Subscribe
+ * Description:         The plugin creates a subscription form and store subscribers into database.
+ * Version:             1.0.0
+ * Requires at least:   4.9
+ * Requires PHP:        7.2
+ * Author:              wppunk
+ * License:             MIT
+ * Text Domain:         subscribe
+ *
+ * @package     Subscribe
+ */
+
+namespace WPPunk\Subscribe;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+define( 'SUBSCRIBE_VERSION', '1.0.0' );
+define( 'SUBSCRIBE_PATH', plugin_dir_path( __FILE__ ) );
+define( 'SUBSCRIBE_URL', plugin_dir_url( __FILE__ ) );
+
+require_once SUBSCRIBE_PATH . 'vendor/autoload.php';
+
+( new Main() )->run();
